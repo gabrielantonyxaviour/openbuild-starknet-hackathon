2ffb7a895b993ab93e271fce9c78c3ac0e0392d9
gabrielantonyxaviour <gabrielantonyxaviour.24ec@licet.ac.in>
2024-01-14 21:15:50 +0530

    add connect button integration working

--- frontend/src/components/modals/ConnectModal.tsx
+++ frontend/src/components/modals/ConnectModal.tsx
@@ -2,11 +2,13 @@ import React from "react";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { faClose, faUmbrella } from "@fortawesome/free-solid-svg-icons";
 import Image from "next/image";
+import { Connector, useConnect } from "@starknet-react/core";
 export default function ConnectModal({
   closeModal,
 }: {
   closeModal: () => void;
 }) {
+  const { connect, connectors } = useConnect();
   return (
     <>
       <div
@@ -14,7 +16,7 @@ export default function ConnectModal({
         onClick={closeModal}
       ></div>
       <div className="fixed top-[50%] left-[50%] -translate-y-1/2 -translate-x-1/2 bg-gradient-to-r from-[#6BFFF7] to-[#FAFB63] p-4 z-50 opacity-100 w-[28%] rounded-lg">
-        <div className="flex justify-between mx-2 mt-2">
+        <div className="flex justify-between mx-2 mt-2 mb-4">
           <p className="text-black font-semibold text-xl">Connect Wallet</p>
           <FontAwesomeIcon
             icon={faClose}
@@ -22,14 +24,22 @@ export default function ConnectModal({
             onClick={closeModal}
           />
         </div>
-        <button className="flex justify-center space-x-4 mt-6 w-full hover:bg-black rounded-xl text-black hover:text-white">
-          <Image src={"/bravoos.png"} width={50} height={50} alt="bravoos" />
-          <p className=" font-semibold  my-auto">Bravoos Wallet</p>
-        </button>
-        <button className="flex justify-center space-x-4 mt-2 mb-6 w-full hover:bg-black rounded-xl text-black hover:text-white">
-          <Image src={"/argent.png"} width={50} height={100} alt="argent" />
-          <p className=" font-semibold  my-auto">Argent Wallet</p>
-        </button>
+        {connectors.map((connector: Connector) => (
+          <button
+            className="flex justify-center space-x-4 my-3 w-full hover:bg-black rounded-xl text-black hover:text-white"
+            key={connector.id}
+            onClick={() => connect({ connector })}
+            disabled={!connector.available()}
+          >
+            <Image
+              src={connector.name == "Braavos" ? "/bravoos.png" : "/argent.png"}
+              width={50}
+              height={50}
+              alt="bravoos"
+            />
+            <p className=" font-semibold  my-auto">{connector.name} Wallet</p>
+          </button>
+        ))}
       </div>
     </>
   );
