b857eb5c47404dd92029179e758daf78c6b7c46b
Gabriel Antony Xaviour <gabrielantony56@gmail.com>
2024-01-15 20:36:48 +0530

    add sidebar layout

--- frontend/src/components/Sidebar.tsx
+++ frontend/src/components/Sidebar.tsx
@@ -1,39 +1,34 @@
 import { faGamepad, faInbox } from "@fortawesome/free-solid-svg-icons";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { useRouter } from "next/router";
-import React, { useEffect } from "react";
+import React, { useEffect, useState } from "react";
+import { usePathname } from "next/navigation";
 
-export default function Sidebar({ selected }: { selected: boolean }) {
+export default function Sidebar() {
   const router = useRouter();
-
-  return selected ? (
+  const pathname = usePathname();
+  return (
     <div className="fixed top-1/2 left-5 transform -translate-y-1/2 h-[200px] ">
       <button
-        className="hover:bg-[#FAFB63] p-5 rounded-full flex hover:text-black text-white bg-black"
+        className={
+          pathname == "/assets" || pathname != "/games"
+            ? "hover:bg-[#FAFB63] p-5 rounded-full flex hover:text-black text-white bg-black"
+            : "bg-[#6BFFF7] p-5 rounded-full flex text-black "
+        }
         onClick={() => {
           router.push("/games");
         }}
+        disabled={pathname == "/games"}
       >
         <FontAwesomeIcon icon={faGamepad} className="text-2xl" />
       </button>
       <button
-        className="bg-[#6BFFF7] p-5 rounded-full flex text-black mt-6 "
-        disabled={true}
-      >
-        <FontAwesomeIcon icon={faInbox} className="text-2xl" />
-      </button>
-    </div>
-  ) : (
-    <div className="fixed top-1/2 left-5 transform -translate-y-1/2 h-[200px] ">
-      <button
-        className="bg-[#6BFFF7] p-5 rounded-full flex text-black "
-        disabled={true}
-      >
-        <FontAwesomeIcon icon={faGamepad} className="text-2xl" />
-      </button>
-
-      <button
-        className="hover:bg-[#FAFB63] p-5 rounded-full flex hover:text-black text-white bg-black mt-6 "
+        className={
+          pathname == "/assets"
+            ? "bg-[#6BFFF7] p-5 rounded-full flex text-black mt-6 "
+            : "hover:bg-[#FAFB63] p-5 rounded-full flex hover:text-black text-white bg-black mt-6 "
+        }
+        disabled={pathname == "/assets"}
         onClick={() => {
           router.push("/assets");
         }}
--- frontend/src/pages/games.tsx
+++ frontend/src/pages/games.tsx
@@ -1,49 +1,11 @@
 import Games from "@/components/Games";
-import Sidebar from "@/components/Sidebar";
-import WalletButton from "@/components/WalletButton";
-import DisconnectModal from "@/components/modals/DisconnectModal";
-import { useAccount } from "@starknet-react/core";
-import Link from "next/link";
-import { useRouter } from "next/router";
+import Layout from "@/components/Layout";
 import React, { useEffect, useState } from "react";
 
 export default function GamesPage() {
-  const { address } = useAccount();
-  const [openDisconnectModal, setOpenDisconnectModal] = useState(false);
-  const router = useRouter();
-  useEffect(() => {
-    console.log("address", address);
-  }, [address]);
-  return address ? (
-    <div>
-      {openDisconnectModal && (
-        <DisconnectModal closeModal={() => setOpenDisconnectModal(false)} />
-      )}
-      <div className="flex justify-between">
-        <Link
-          href={"/"}
-          className="ml-8 mt-6 font-bold font-logo  text-3xl italic text-[#FAFB63] "
-        >
-          StarkGG
-        </Link>
-        <WalletButton
-          address={address}
-          onClick={() => {
-            setOpenDisconnectModal(true);
-          }}
-        />
-      </div>
-      <Sidebar selected={false} />
+  return (
+    <Layout>
       <Games />
-    </div>
-  ) : (
-    <div className="absolute top-1/2 left-1/2 transform -translate-x-1/2 -translate-y-1/2 ">
-      <Link
-        href={"/"}
-        className="mt-20 bg-gradient-to-r from-[#6BFFF7] to-[#FAFB63] rounded-md p-3 font-bold italic text-lg text-black"
-      >
-        CONNECT WALLET
-      </Link>
-    </div>
+    </Layout>
   );
 }
